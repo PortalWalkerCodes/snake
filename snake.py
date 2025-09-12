73661d0b865eb6af8d6a0c981853572d76d7e73f
Atticus <atticus.beard@icloud.com>
2025-09-12 07:52:25 -0600

    Added food.py and made the snake MOVE!

--- snake.py
+++ snake.py
@@ -1,20 +1,18 @@
 from turtle import Turtle
 
 MOVE_DISTANCE = 20
-
 class Snake:
     def __init__(self):
 
         self.segments = []
         self.x_cor = 0
         self.create_snake()
+        self.head = self.segments[0]
+        self.UP = 90
+        self.DOWN = 270
+        self.LEFT = 180
+        self.RIGHT = 0
 
-    def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(x=new_x, y=new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
     def create_snake(self):
         for _ in range(3):
             new_segment = Turtle("square")
@@ -23,3 +21,26 @@ class Snake:
             new_segment.goto(x=self.x_cor, y=0)
             self.x_cor -= 20
             self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x=new_x, y=new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def left(self):
+        if self.head.heading() != self.RIGHT:
+            self.head.setheading(self.LEFT)
+
+    def right(self):
+        if self.head.heading() != self.LEFT:
+            self.head.setheading(self.RIGHT)
+
+    def down(self):
+        if self.head.heading() != self.UP:
+            self.head.setheading(self.DOWN)
+
+    def up(self):
+        if self.head.heading() != self.DOWN:
+            self.head.setheading(self.UP)
